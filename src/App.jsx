45ba19c0076e71301428f202ace262e58b7e3ca4
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-06-16 16:17:07 +0100

    installed react-router-dom and set up navbar links and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import CreateBlog from "./CreateBlog";
+
+// Importing everything needed for using React Router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
 	return (
-		<>
+		// Any components that you want to use routing with need to be wrapped with the Router component
+		<Router>
 			<div className=" max-w-2xl mx-auto px-2">
 				<Navbar />
-				<Home />
+				{/* The switch component makes sure that only one route is being shown at any one time */}
+				<Switch>
+					{/* The Route component is responsible for defining the route path */}
+					{/* The value of the path property will tacked onto the end of your domain name */}
+					<Route exact path="/">
+						{/* You then set the component that you want rendered when that path has been navigated to */}
+						<Home />
+					</Route>
+					<Route path="/create">
+						<CreateBlog />
+					</Route>
+				</Switch>
 			</div>
-		</>
+		</Router>
 	);
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
 	return (
 		<header className="py-2 border-b-2 border-pink-100">
 			<nav className="flex justify-between items-center">
 				<h1 className="text-xl text-pink-500 font-medium">Joe's Blogs</h1>
 				<div className="flex gap-6">
-					<a href="#" target="_blank" rel="noopener noreferrer">
-						Home
-					</a>
-					<a href="#" target="_blank" rel="noopener noreferrer">
-						New Blog
-					</a>
+					<Link to="/">Home</Link>
+					<Link to="/create">New Blog</Link>
 				</div>
 			</nav>
 		</header>
