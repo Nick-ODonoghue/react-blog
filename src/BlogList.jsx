ee951d47077c119e1d6d5e4ee12822ece79ab858
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-06-16 15:07:52 +0100

    setup custom useFetch hook

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 const BlogList = ({ blogs, title, handleDelete }) => {
 	return (
 		<>
-			<h2 className=" text-xl font-medium mb-2">{title}</h2>
+			<h2 className=" text-xl font-semibold mb-2">{title}</h2>
 			{blogs.map((blog) => (
-				<div key={blog.id} className="mb-6 p-2 rounded-md hover:shadow-md transition-all duration-200 cursor-pointer">
+				<div
+					key={blog.id}
+					className="mb-4 px-2 py-4 rounded-md hover:shadow-md transition-all duration-200 cursor-pointer"
+				>
 					<h3 className=" text-pink-500 font-medium text-lg">{blog.title}</h3>
 					<p className=" text-sm">Written by {blog.author}</p>
-					<button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+					{/* <button
+						className="border border-pink-500 py-1 px-2 mt-2 rounded-full text-xs shadow-md hover:bg-pink-500 hover:text-white hover:border-pink-500 hover:shadow-none transition-all duration-200"
+						onClick={() => handleDelete(blog.id)}
+					>
+						Delete blog
+					</button> */}
 				</div>
 			))}
 		</>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: "Blog 1", body: "lorem ipsum...", author: "NickOD.xyz", id: 1 },
-		{ title: "Blog 2", body: "lorem ipsum...", author: "NickOD.xyz", id: 2 },
-		{ title: "Blog 3", body: "lorem ipsum...", author: "NickOD.xyz", id: 3 },
-	]);
+	// const handleDelete = (id) => {
+	// 	const newBlogs = blogs.filter((blog) => blog.id !== id);
+	// 	setBlogs(newBlogs);
+	// };
 
-	const handleDelete = (id) => {
-		console.log(id);
-		const newBlogs = blogs.filter((blog) => {
-			return blog.id !== id;
-		});
-		setBlogs(newBlogs);
-	};
+	const { data: blogs, isLoading, error } = useFetch("http://localhost:8000/blogs");
 
 	return (
 		<main className="py-6">
-			<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+			{error && <div>{error}</div>}
+			{isLoading && <div>Loading blogs...</div>}
+			{blogs && <BlogList blogs={blogs} title="All Blogs" />}
 		</main>
 	);
 };
