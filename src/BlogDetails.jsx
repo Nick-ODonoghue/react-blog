5cede52079e616c2d575789733a24510eb973885
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-06-19 16:24:19 +0100

    added new blog component and logic

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 	const { id } = useParams();
+	const { data: blog, isLoading, error } = useFetch("http://localhost:8000/blogs/" + id);
 
 	return (
 		<div className=" py-6">
-			<h2 className=" text-xl font-semibold mb-2">Blog Details {id}</h2>
+			{/* <h2 className=" text-xl font-semibold mb-2">Blog Details {id}</h2> */}
+			{isLoading && <div>Loading blog...</div>}
+			{error && <div>{error}</div>}
+			{blog && (
+				<article>
+					<h2 className=" text-xl font-semibold mb-2">{blog.title}</h2>
+					<h3 className=" text-sm font-light mb-8">Written by {blog.author}</h3>
+					<p className=" text-md font-normal">{blog.body}</p>
+				</article>
+			)}
 		</div>
 	);
 };
--- src/CreateBlog.jsx
+++ src/CreateBlog.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const CreateBlog = () => {
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
+	const [author, setAuthor] = useState("");
+	const [isPending, setIsPending] = useState(false);
+	const history = useHistory();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const blog = { title, body, author };
+
+		setIsPending(true);
+
+		fetch("http://localhost:8000/blogs", {
+			method: "POST",
+			headers: { "content-Type": "application/json" },
+			body: JSON.stringify(blog),
+		}).then(() => {
+			console.log("new blog added");
+			setIsPending(false);
+			history.push("/");
+		});
+	};
+
 	return (
 		<div className=" py-6">
 			<h2 className=" text-xl font-semibold mb-2">Add a New Blog</h2>
+			<form onSubmit={handleSubmit} className="flex flex-col">
+				<label className="mb-2">Blog title:</label>
+				<input className="mb-4 p-2" type="text" required onChange={(e) => setTitle(e.target.value)} />
+				<label className="mb-2">Blog body:</label>
+				<textarea className="mb-4 p-2" required onChange={(e) => setBody(e.target.value)}></textarea>
+				<label className="mb-2">Author:</label>
+				<input className="mb-4 p-2" type="text" onChange={(e) => setAuthor(e.target.value)} />
+				<button className="border border-pink-500 bg-pink-500 text-white py-1 px-2 mt-2 rounded-full text-xs font-medium shadow-md hover:bg-black hover:text-pink-500 hover:border-pink-500 hover:shadow-none hover:font-semibold transition-all duration-200 w-1/2 mx-auto">
+					{isPending && "Loading blog"}
+					{!isPending && "Add blog"}
+				</button>
+			</form>
 		</div>
 	);
 };
